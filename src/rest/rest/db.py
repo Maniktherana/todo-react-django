1ab3e3cd08b617f051f0c8627467e9eef6a23ccf
Manik Rana <manikrana54@gmail.com>
2023-10-19 19:20:46 +0530

    lint code

--- src/rest/rest/db.py
+++ src/rest/rest/db.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 
+
 class MongoConnection:
     def __init__(self, host, port, db_name, collection_name):
         mongo_uri = f'mongodb://{host}:{port}'
         self.db = MongoClient(mongo_uri)[db_name]
         self.collection = self.db[collection_name]
+        
