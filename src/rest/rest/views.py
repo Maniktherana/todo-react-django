9ce34e0ed47f864a87584f82bdb4232aa27ea835
Manik Rana <manikrana54@gmail.com>
2023-10-19 19:20:18 +0530

    remove unused imports
    -lint code

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-import json, logging, os
-
+import logging, os
 from dotenv import load_dotenv
-from pymongo import MongoClient
 
 from rest.db import MongoConnection
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+
+dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
 load_dotenv(dotenv_path)
 
 
@@ -18,37 +16,54 @@ class TodoListView(APIView):
     def __init__(self, *args, **kwargs):
         super(TodoListView, self).__init__(*args, **kwargs)
         self.mongo_connection = MongoConnection(
-            os.environ["MONGO_HOST"], 
-            os.environ["MONGO_PORT"], 
-            os.environ["DATABASE_NAME"], 
-            os.environ["COLLECTION_NAME"], 
+            os.environ["MONGO_HOST"],
+            os.environ["MONGO_PORT"],
+            os.environ["DATABASE_NAME"],
+            os.environ["COLLECTION_NAME"],
         )
 
     def get(self, request):
         try:
             todos = self.mongo_connection.collection.find({})
             serialized_todos = [self.serialize_todo(todo) for todo in todos]
-            return Response({'todos': serialized_todos}, status=status.HTTP_200_OK)
+            return Response({"todos": serialized_todos}, status=status.HTTP_200_OK)
         except Exception as e:
             logging.error(f"An error occurred while retrieving todos: {e}")
-            return Response({'error': 'Internal Server Error', "hello": os.environ["DATABASE_NAME"] }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                {
+                    "error": "Internal Server Error",
+                    "hello": os.environ["DATABASE_NAME"],
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
     def post(self, request):
         try:
             data = request.data
-            todo_item = data.get('todo', None)
+            todo_item = data.get("todo", None)
             if todo_item:
-                result = self.mongo_connection.collection.insert_one({'todo': todo_item})
+                result = self.mongo_connection.collection.insert_one(
+                    {"todo": todo_item}
+                )
                 inserted_id = str(result.inserted_id)
-                return Response({'id': inserted_id, 'todo': todo_item}, status=status.HTTP_201_CREATED)
+                return Response(
+                    {"id": inserted_id, "todo": todo_item},
+                    status=status.HTTP_201_CREATED,
+                )
             else:
-                return Response({'error': 'Request data must contain a todo item'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Request data must contain a todo item"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
         except Exception as e:
             logging.error(f"An error occurred while adding a todo: {e}")
-            return Response({'error': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                {"error": "Internal Server Error"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
     @staticmethod
     def serialize_todo(todo):
         serialized_todo = todo.copy()
-        serialized_todo['_id'] = str(todo['_id'])
+        serialized_todo["_id"] = str(todo["_id"])
         return serialized_todo
