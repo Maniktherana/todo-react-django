55b365789f9230293fe73f6b897962c7ba6cf32f
Manik Rana <manikrana54@gmail.com>
2023-10-19 19:16:20 +0530

    implement backend routes
    -Add python-dotenv package
    -Move db init to db.py
    -Implement a get and post route
    -Add a serializer

---
+++ src/rest/rest/db.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+class MongoConnection:
+    def __init__(self, host, port, db_name, collection_name):
+        mongo_uri = f'mongodb://{host}:{port}'
+        self.db = MongoClient(mongo_uri)[db_name]
+        self.collection = self.db[collection_name]
